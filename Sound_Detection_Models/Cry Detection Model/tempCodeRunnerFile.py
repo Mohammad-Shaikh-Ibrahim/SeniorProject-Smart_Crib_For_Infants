e7349e1f803cd09ba0c967b9052f9a5a015daacb
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2025-03-31 18:25:10 +0300

    Update

---
+++ Sound_Detection_Models/Cry Detection Model/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+new_file_path = 'path_to_new_audio_file.wav'
+# new_features = extract_features(new_file_path).reshape(1, -1)  # Reshape to match input shape
+# prediction = model.predict(new_features)
+# predicted_label = label_encoder.inverse_transform([np.argmax(prediction)])
+# print("Predicted class:", predicted_label)
